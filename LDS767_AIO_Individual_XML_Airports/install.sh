87af38a957f7a606f4a21a477695bec06150eea4
Tobias Dammers <tdammers@gmail.com>
2018-11-21 14:58:31 +0100

    Improved EDLN and EHRD SIDs

--- LDS767_AIO_Individual_XML_Airports/install.sh
+++ LDS767_AIO_Individual_XML_Airports/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 BASEDIR=$(dirname "$0")
+# DESTDIR="$HOME/.fgfs/TerraSync"
+DESTDIR="$HOME/flightgear/fgdata-standalone/Scenery/fgx-airports"
 AIRPORTS=$(cat "$BASEDIR/APlist.txt"|grep '[A-Z]\{4\}')
 for AIRPORT in $AIRPORTS
 do
@@ -7,8 +9,10 @@ do
     DIR1=$(echo "$AIRPORT" | cut -c 1)
     DIR2=$(echo "$AIRPORT" | cut -c 2)
     DIR3=$(echo "$AIRPORT" | cut -c 3)
-    DST="$HOME/.fgfs/TerraSync/Airports/$DIR1/$DIR2/$DIR3/$AIRPORT.procedures.xml"
+    DST="$DESTDIR/Airports/$DIR1/$DIR2/$DIR3/$AIRPORT.procedures.xml"
     SRC="$BASEDIR/$AIRPORT.xml"
+    echo "mkdir -p "\""$(dirname "$DST")"\"
+    mkdir -p "$(dirname "$DST")"
     echo "$SRC -> $DST"
     rm -f "$DST"
     cp "$SRC" "$DST"
